28f053f9986d2b5c1540b1d521d7a73ffd29cd80
raghavsaurabh337-arch <raghavsaurabh337@gmail.com>
2026-04-14 18:44:05 +0530

    (let var const) and Data type

--- first.js
+++ first.js
@@ -1,1 +1,62 @@
 console.log("Hello world!");
+console.log("i love js");
+fullname ="saurabh";
+console.log(fullname);
+
+
+price=123.67;
+console.log(price);
+x=null;
+y=undefined;
+console.log(x,y);
+/*boolean type */
+
+infollow=false;
+console.log(infollow)
+fullname=25;
+ 
+
+
+/* let & const var*/
+let full_name="saurabh";
+let age =20;
+let total_price=10000;
+console.log(full_name, age, total_price);    
+{
+let a;
+console.log(a);
+a=20;
+console.log(a);
+}
+{
+     let a=null;
+     console.log(typeof(a));  
+}
+const z = BigInt("123")
+
+/*object */
+const student ={
+     name:"saurabh",
+     age:20,
+     cgpa:8.2,
+     isPass:true,
+     "learning status":"good",
+     isPass:true,
+}
+console.log(student.age)
+
+const product={
+     title:"ball pen",
+     rating:4,
+     offer:5,
+     price:270
+};
+console.log(product);
+
+const profile={
+     user_name:"saurabh Raghav",
+     isfollow:false,
+     followers:123,
+     following:123,
+};
+console.log(profile);
