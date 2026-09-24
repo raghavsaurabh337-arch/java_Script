4d9d6af830ac6bae0d5ed55064be7e3db7dfc924
raghavsaurabh337-arch <raghavsaurabh337@gmail.com>
2026-09-24 12:00:51 +0530

    conversion DataType

---
+++ Chai aur Code/conversionOperation.js
@@ -0,0 +1,29 @@
+let score="saurabh"
+console.log(typeof score)
+
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber)
+console.log(typeof score)
+console.log(valueInNumber)
+
+
+// "33" =>33
+// "33ab"=> Nan
+// true=> 1
+// undefined=> Nan
+// string -> NaN
+
+
+let isLoggedIn="mek"
+let booleanIsLogedIn=Boolean(isLoggedIn)
+console.log(booleanIsLogedIn)
+
+// Boolean
+
+// 1=> True
+// 0=> False
+// "1"=> true
+// "saurabh" => true
+// ""=> false
+
