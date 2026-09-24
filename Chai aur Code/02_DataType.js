374ac1fb68edf84978d65dcb490d4176be6dd106
raghavsaurabh337-arch <raghavsaurabh337@gmail.com>
2026-09-24 11:40:09 +0530

    varibale and datatype

---
+++ Chai aur Code/02_DataType.js
@@ -0,0 +1,21 @@
+// alert(3+3)  we are not using node,js and browser
+console.log(2+
+    
+    2)  // code readability should be high
+
+console.log(3+4)    
+console.log("saurabh singh raghav")
+
+// number => 2 to power 25
+// string = ""
+//  boolean =True/ False 
+//  null=> standalone valuse
+//  undefined  =>
+// symbol => unique
+
+
+
+// objects 
+
+console.log(typeof null) // types object 
+console.log(typeof undefined) // types undefined
---
+++ Chai aur Code/varibale.js
@@ -0,0 +1,10 @@
+const accountId = "123456";
+let accountBalance = 10000;
+var accountType = "savings";
+let accountStatus ;
+let accountName="saurabh raghav"
+
+
+accountBalance += 5000; // Adding 5000 to the account balance
+// accountId=43232
+console.table([accountId, accountBalance, accountType, accountStatus,accountName]);
