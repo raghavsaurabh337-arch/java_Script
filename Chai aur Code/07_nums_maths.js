4281abcac1eaf60694f02e6d09714855d2829f14
raghavsaurabh337-arch <raghavsaurabh337@gmail.com>
2026-09-25 12:13:59 +0530

    math and num

---
+++ Chai aur Code/07_nums_maths.js
@@ -0,0 +1,25 @@
+const score = 200
+console.log(score)
+
+const AcBal=new Number(399)
+console.log(AcBal)
+console.log(AcBal.toString())
+console.log(AcBal.toString().length)
+console.log(AcBal.toFixed(3))
+
+const hunderds=1000000000
+console.log(hunderds.toLocaleString('en-IN'));
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++  MAth +++++++++++++++++++++++++++++++++++++
+
+const num=4;
+console.log(Math);
+console.log(Math.abs(-5));
+console.log(Math.round()); // 0 to 1 
+console.log((Math.random()*10)+1);// 1 to 9
+console.log(Math.floor((Math.random()*10))+1);
+
+const min =10
+const max =20
+console.log(Math.floor(Math.random()*(max-min+1))+min);
