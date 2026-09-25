bc0ffacf20b7a9ce613b39786974b1d2ba1b63d5
raghavsaurabh337-arch <raghavsaurabh337@gmail.com>
2026-09-25 16:34:48 +0530

    Date in js

---
+++ Chai aur Code/08-DateJs.js
@@ -0,0 +1,25 @@
+let myDate = new Date()
+// console.log(myDate)
+// console.log(myDate.toString())
+// console.log(myDate.toISOString())
+// console.log(myDate.toDateString())
+
+
+
+
+let myCreateDate = new Date(2027,9,3)
+let myCreatDate = new Date("01-03-2025")
+console.log("DATE: ",myCreatDate);
+// console.log(myCreateDate)
+// console.log(myCreateDate.toString())
+// console.log(myCreateDate.toISOString())
+// console.log(myCreateDate.toDateString())
+
+
+
+//  Time ++++++++++++++++++++++++
+
+let myTime=Date.now()
+console.log(myTime)
+console.log(myCreatDate.getTime())
+console.log(Math.floor(Date.now()/1000))
