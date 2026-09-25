64119929627a3e295daa81a70af0eac9b19b455e
raghavsaurabh337-arch <raghavsaurabh337@gmail.com>
2026-09-25 11:05:13 +0530

    Data type of summy and memory

---
+++ Chai aur Code/04_comparision.js
@@ -0,0 +1,12 @@
+console.log(2>4)
+// console.log(2=4)
+console.log(2==4)
+console.log(2<=4)
+console.log(3!=1)
+
+console.log(typeof "3">1)
+console.log(3<"23")
+
+console.log(null>0);
+console.log(null>=0);
+console.log(null==0);
---
+++ Chai aur Code/05_DataType_summay.js
@@ -0,0 +1,34 @@
+// primitive data type
+
+//  7 types :-  number , Boolean string ,  BigIn , undefined, null,Symbol
+
+// non-primivite 
+    // Array, function,Object  =>  typeof objecet
+
+    //  ****************************************************memory*******************************
+    // stack  (primivite )=> copy value , heap (non- primivite)=> origin value
+
+
+
+    let nameFull ="saurabh RAghav"
+    let FirstName = nameFull
+    let anotherName="lalit raghav"
+    console.log(nameFull);
+    console.log(anotherName);
+
+
+
+    let userOne={
+        email:"saurabh@gmail.com",
+        upi:"upi@ybl"
+
+    }
+   userTwo= userOne
+    userTwo.email="raghav@gmail.com"
+    console.log(userOne.email);
+    console.log(userTwo.email);
+       
+
+
+
+
